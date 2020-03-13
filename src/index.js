31a93b3c09983a0e9227dfb518d7d82510995b31
Daniel Armas <daniel62armas@gmail.com>
2020-03-12 20:54:41 -0600

    Component complete

--- src/index.js
+++ src/index.js
@@ -5,34 +5,10 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const user = {
-    firstName: 'Daniel',
-    lastName: 'Armas',
-    avatar: 'https://image.flaticon.com/icons/svg/1674/1674292.svg'
-}
-
-function getName(user){
-    return '${user.firstName} ${user.lastName}'
-}
-
-function getGretting(user){
-    if(user){
-        return <h1>Hello {getName(user)} </h1>
-    }
-    return <h1>Hello Stranger </h1>
-
-}
-
-const element = (
-    <div>
-        <h1>{getGretting(user)}</h1>
-        <img src={user.avatar} />
-    </div>
-)
+import Card from './components/card'
 
 const container = document.getElementById('root')
 
 //ReactDOM.render(__QUE__, __DONDE__)
 
-ReactDOM.render(element, container)
+ReactDOM.render(<Card />, container)
