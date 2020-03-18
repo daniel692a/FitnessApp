f3ff3cdb75a548be62b9fdb8caf1712a2ca6ddd2
Daniel Armas <daniel62armas@gmail.com>
2020-03-17 23:44:36 -0600

    Create Function Components

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles/Welcome.css'
+
+//Componentes funcionlaes, porque son hechos con funciones de JS
+function Welcome(props){
+    return(
+        <div className="container">
+            <div className="Fitness-User-Info">
+                <h1>Hello {props.username}</h1>
+                <p>Let´s workout to get someones gains!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
--- src/components/card.js
+++ src/components/card.js
@@ -4,18 +4,33 @@ import './styles/card.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 class Card extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            image: 'https://image.flaticon.com/icons/svg/1674/1674292.svg'
+        }
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({
+                image: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'
+            })
+        }, 3000)
+    }
     render(){
         const { title, description, img, leftColor, rightColor } = this.props
         return (
             <div className='card mx-auto Fitness-Card'
             style={{
-                backgroundImage: 'url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})'
+                backgroundImage: 'url('+circlesImg+'), linear-gradient(to right, '+leftColor+', '+rightColor+')'
             }}
             >
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={img} className="float right"/>
+                            <img src={this.state.image} className="float right"/>
                         </div>
                     <div className="col-6 Fitness-Card-Info">
                         <h1>{title}</h1>
