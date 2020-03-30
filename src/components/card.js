3bceab9d17486bccec4047440538c7944e9c7fcf
Daniel Armas <daniel62armas@gmail.com>
2020-03-30 12:52:44 -0600

    Edit components an add Pages

--- src/components/card.js
+++ src/components/card.js
@@ -30,7 +30,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={this.state.image} className="float right"/>
+                            <img src={this.state.image} className="float-right"/>
                         </div>
                     <div className="col-6 Fitness-Card-Info">
                         <h1>{title}</h1>
